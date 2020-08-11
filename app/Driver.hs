dcffe18bec0b8d2a693fd041f25de7108b904155
archaephyrryx <caufeminecraft+github@gmail.com>
2020-08-11 18:07:31 -0400

    Multithreaded linemode zepto parser
    Linemode now spawns worker threads to perform Zepto-based parsing
    on line-chunked input
    
    Removed app/MainTest.hs and extraneous code in app/Driver.hs

--- app/Driver.hs
+++ app/Driver.hs
@@ -1,6 +1,10 @@
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE MultiWayIf #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE RecordWildCards #-}
 
 module Driver where
 
@@ -8,7 +12,6 @@ import qualified Data.Attoparsec.ByteString as A
 
 import Data.Semigroup ((<>))
 
-import Control.Monad (void)
 import Control.Monad.IO.Class (MonadIO(..))
 
 import qualified Data.ByteString as B
@@ -28,8 +31,7 @@ import Data.Conduit ((.|))
 import qualified Data.ByteString.Streaming as BS
 import qualified Data.ByteString.Streaming.Char8 as BS8
 
-import Streaming (Of(..))
-import qualified Streaming as S
+import Streaming
 import qualified Streaming.Prelude as S
 
 
@@ -37,18 +39,40 @@ import Data.Maybe (isJust, fromJust, mapMaybe)
 import Data.Either (isRight, fromRight, isLeft, fromLeft)
 import Data.List (stripPrefix)
 
-import JParse hiding (zeptoC)
+import JParse
 import Parse
 
 import qualified Parse.ReadAlt as Alt
-import qualified Parse.ReadZepto as Zep
-import qualified Parse.ReadStream as ZepS
 
+import qualified Parse.ReadZepto as Zep
 import qualified Parse.Parser.Zepto as Z
+
+import qualified Parse.ReadStream as ZepS
 import qualified Parse.Parser.ZeptoStream as ZS
 
+-- Concurrency mode
+import Control.Concurrent
+import Control.Concurrent.Chan
+import Control.Concurrent.STM
+import Control.Concurrent.STM.TVar
+import Control.Monad (replicateM_, unless, when)
+import System.IO (stdout)
+import System.Environment
 
-data Mode = BlockMode | LineMode | DebugMode deriving (Eq)
+
+-- * Command-line specification of input processing mode
+
+-- | Algebraic data type encapsulating the two primary modes of operation:
+--
+-- * @BlockMode@ for data that is either formatted with multiple objects per line, multiple lines per object, or arbitrarily long lines that are not safe to read into memory as a unit.
+--
+-- * @LineMode@ for (theoretically) more efficient parsing of data where every line contains exactly one complete JSON object, and lines are trusted to be sufficiently short to permit reading entire lines into memory as a unit.
+--
+-- * Additionally supports @DebugMode@ for human-readable printing of the raw return values of line-mode parser, rather than printing them in a machine-readable way after filtering and eliminating extraneous constructors around the actual result value.
+data Mode = BlockMode -- ^ Attoparsec inside Conduit
+          | LineMode -- ^ Zepto inside Streaming
+          | DebugMode -- ^ Zepto inside Streaming (verbose)
+          deriving (Eq)
 
 getKeyMode :: [String] -> (ByteString, Mode)
 getKeyMode xs = (qkey xs, getMode xs)
@@ -64,20 +88,143 @@ getMode xs =
               | otherwise -> error $ "unrecognized mode \""++x++"\""
 {-# INLINE getMode #-}
 
+-- * LineMode specialization
+
+-- | number of lines per individual sub-stream in line-mode
+nLines :: Int
+nLines = 1024
+{-# INLINE nLines #-}
+
+-- | number of worker threads to run Zepto parsing in parallel
+nWorkers :: IO Int
+nWorkers = getNumCapabilities
+{-# INLINE nWorkers #-}
+
+-- | Upper bound on number of unprocessed items in a channel
+uBound :: Int
+uBound = 1000
+{-# INLINE uBound #-}
+
+streamZepto :: Z.Parser (Maybe Builder) -> IO ()
+streamZepto = zeptoMain
+
+-- | set of common synchronization values for concurrent linemode
+data ZEnv
+   = ZEnv
+     { nworkers :: Int -- ^ number of workers (runtime)
+     , nw :: TVar Int -- ^ number of unterminated worker threads
+     , done :: MVar () -- ^ signal for end-of-processing
+     , input :: Chan [L.ByteString] -- ^ channel for unparsed input
+     , inCap :: TVar Int -- ^ counter for items in input channel
+     , output :: Chan (Maybe ByteString) -- ^ channel for parsed output
+     , outCap :: TVar Int -- ^ counter for items in output channel
+     }
 
 
 
-{-
-zeptoC :: Monad m => String -> C.ConduitT ByteString ByteString m ()
-zeptoC str = C.mapC (run' (keyToZepto str) $ build.fromJust)
--}
+newZEnv :: IO ZEnv
+newZEnv = do
+  nworkers <- nWorkers
+  nw <- newTVarIO nworkers
+  inCap <- newTVarIO 0
+  outCap <- newTVarIO 0
+  done <- newEmptyMVar
+  input <- newChan
+  output <- newChan
+  return ZEnv{..}
 
-streamZepto :: MonadIO m => Z.Parser (Maybe Builder) -> m ()
-streamZepto z = S.mapM_ putLnBuilder $ trimZepto $ streamZ z streamlines
+
+zeptoMain :: Z.Parser (Maybe Builder) -> IO ()
+zeptoMain z = do
+  ZEnv{..} <- newZEnv
+  forkIO $ distributor input inCap
+  mapM_ (\_ -> forkIO $ worker input output inCap outCap nw z) [1..nworkers]
+  forkIO $ collector output outCap done
+  monitor output nw
+  readMVar done
+
+monitor :: Chan (Maybe a) -> TVar Int -> IO ()
+monitor output nw = do
+  atomically $ do
+    n <- readTVar nw
+    unless (n == 0) retry
+  writeChan output Nothing
+
+distributor :: Chan [L.ByteString] -> TVar Int -> IO ()
+distributor input inCap = do
+  S.mapM_ (doWrite input inCap) listStream
+  writeChan input []
+
+collector :: Chan (Maybe ByteString) -> TVar Int -> MVar () -> IO ()
+collector output outCap done = go
+  where
+    go = do
+      doRead output outCap >>= \case
+        Just bs -> B.putStr bs >> go
+        Nothing -> putMVar done ()
+
+
+-- Roundabout BoundedChannel
+
+doWrite :: Chan a -> TVar Int -> a -> IO ()
+doWrite chan capt val = do
+  atomically $ do
+    level <- readTVar capt
+    when (level >= uBound) retry
+  atomically $ modifyTVar capt succ
+  writeChan chan val
+
+doRead :: Chan a -> TVar Int -> IO a
+doRead chan capt = do
+  atomically $ modifyTVar capt pred
+  readChan chan
+
+worker :: Chan [L.ByteString]
+       -> Chan (Maybe ByteString)
+       -> TVar Int
+       -> TVar Int
+       -> TVar Int
+       -> Z.Parser (Maybe Builder)
+       -> IO ()
+worker input output inCap outCap nw z = go
+  where
+    go = do
+      bss <- doRead input inCap
+      if null bss
+         then do
+           atomically $ modifyTVar nw pred
+           writeChan input []
+         else labor output outCap z bss >> go
+
+labor :: Chan (Maybe ByteString)
+      -> TVar Int
+      -> Z.Parser (Maybe Builder)
+      -> [L.ByteString]
+      -> IO ()
+labor output outCap z lbs = do
+  let bld = foldr (accZepto z) (mempty :: Builder) lbs
+      !bs = build bld
+  doWrite output outCap (Just bs)
+
+accZepto :: Z.Parser (Maybe Builder)
+         -> L.ByteString
+         -> Builder
+         -> Builder
+accZepto z bs bld =
+  case Z.parse z (L.toStrict bs) of
+    Right (Just x) -> x <> D.word8 0x0a <> bld
+    _ -> bld
 
 debugZepto :: MonadIO m => Z.Parser (Maybe Builder) -> m ()
 debugZepto z = S.mapM_ (liftIO . Prelude.putStrLn) $ debugPrint $ streamZ z streamlines
 -- debugZepto z = linestream $ streamlines
+
+
+chunkStream :: MonadIO m => Stream (Stream (Of L.ByteString) m) m ()
+chunkStream = chunksOf nLines $ mapped BS.toLazy streamlines
+
+listStream :: Stream (Of [L.ByteString]) IO ()
+listStream = mapped S.toList chunkStream
 
 debugPrint :: MonadIO m
            => S.Stream (Of (Either String (Maybe Builder))) m r
@@ -87,148 +234,25 @@ debugPrint = S.map (either ("Left>"++) buildString)
 buildString Nothing = "Right>Nothing"
 buildString (Just d) = "Right>Just>" ++ S8.unpack (build d)
 
-trimZepto :: MonadIO m => S.Stream (Of (Either String (Maybe Builder))) m r -> S.Stream (Of (Maybe Builder)) m r
-trimZepto = S.map (fromRight Nothing) . S.filter isRight
-
 purgeZepto :: MonadIO m => S.Stream (Of (Either String (Maybe Builder))) m r -> S.Stream (Of String) m r
 purgeZepto = S.map (fromLeft "weird...") . S.filter isLeft
 
 streamZ :: MonadIO m => Z.Parser (Maybe Builder)
         -> S.Stream (BS8.ByteString m) m r
         -> S.Stream (Of (Either String (Maybe Builder))) m r
-streamZ z = S.map (parseStream z) . S.mapped BS.toLazy
+streamZ z = S.map (parseStream z) . mapped BS.toLazy
 
 parseStream :: Z.Parser (Maybe Builder)
             -> L.ByteString
             -> Either String (Maybe Builder)
 parseStream z = Z.parse z . L.toStrict
 
-streamParse :: MonadIO m
-            => Z.Parser (Maybe Builder)
-            -> BS8.ByteString m r
-            -> m (Of (Either String (Maybe Builder)) r)
-streamParse z bs = do
-  bs' <- BS.toStrict_ (bsVoid bs)
-  let val = Z.parse z bs'
-  res <- BS.effects bs
-  return $ val :> res
-
-bsVoid :: Monad m => BS8.ByteString m r -> BS8.ByteString m ()
-bsVoid = BS8.takeWhile (const True)
-{-# INLINE bsVoid #-}
-
-linestream :: MonadIO m => S.Stream (BS8.ByteString m) m () -> m ()
-linestream = BS.stdout . BS8.unlines
-
 streamlines :: MonadIO m => S.Stream (BS8.ByteString m) m ()
 streamlines = BS8.lines BS.stdin
-
-run :: A.Parser a -> (a -> b) -> ByteString -> b
-run p f b = (\(A.Done _ x) -> f x) $ A.parse p b
-
-run' :: Z.Parser a -> (a -> b) -> ByteString -> b
-run' p f b = (\(Right x) -> f x) $ Z.parse p b
-
-runZ :: ZS.Parser a -> (a -> b) -> ByteString -> b
-runZ p f b = (\(Right x) -> f x) $ ZS.parse p b
-
-bstring :: ByteString
-bstring = "\"this is an exceptionally long string with escapes such as \\b, \\t, \\\\, and even \\u2f3F-like quads\""
-
-bstring' :: ByteString
-bstring' = "\"this is an exceptionally long string with a single quad and no other escapes. the quad is \\u2f3f and everything else is just plain text. really nothing too interesting.\""
-
-fooson :: ByteString
-fooson = "{ \"ignore\" : -123 , \"everything\":null, \"until\":[\"you\",\"see\"] , \"the\":{ \"key\":\"foo\" }, \"foo\":\"<- here it is!\" }"
-
-countParse :: String -> String -> IO Int
-countParse str inp =
-  let parser = keyToParser str
-   in C.runResourceT $ runParseWithC (C.lengthIfC isJust) (A.parse parser) (C.sourceFile inp)
-
-countParse' :: String -> String -> IO Int
-countParse' str inp =
-  let parser = keyToParser' str
-   in C.runResourceT $ runParseWithC (C.lengthIfC isJust) (A.parse parser) (C.sourceFile inp)
-
-countZepto :: String -> String -> IO Int
-countZepto str inp =
-  let parser = keyToZepto str
-   in C.runResourceT $ runZeptoWithC (C.lengthIfC isJust) (Z.parseR parser) (C.sourceFile inp)
-
-
-_count = 100000
-_len = 10
-
-_hrun, _lrun, _mrun :: IO ()
-_hrun = C.runResourceT $ C.runConduit $ _hsource .| C.takeC _count .| C.unlinesAsciiC .| C.sinkFile "heads.txt"
-_lrun = C.runResourceT $ C.runConduit $ _lsource .| C.takeC _count .| C.unlinesAsciiC .| C.sinkFile "lasts.txt"
-_mrun = C.runResourceT $ C.runConduit $ _msource .| C.takeC _count .| C.unlinesAsciiC .| C.sinkFile "mids.txt"
-
-
-_hsource = C.repeatC $! _hed _len
-_lsource = C.repeatC $! _lst _len
-_msource = C.repeatC $! _mid _len
-
-
-
-_hed = (\n -> genObj "foo" 0 n)
-_lst = (\n -> genObj "foo" (n-1) n)
-_mid = (\n -> genObj "foo" (n`div`2) n)
-
-
--- takes a key value, position of key, and number of values and generates generic simple JSON object
-genObj :: ByteString -> Int -> Int -> ByteString
-genObj bs i n = build $  "{ " <> genObj' bs i n <> " }"
-  where
-    genObj' :: ByteString -> Int -> Int -> Builder
-    genObj' bs 0 1 = "\""<>D.byteString bs<>"\":\"bar\\f\\udead\\ubeef\""
-    genObj' bs _ 1 = "\"baz\":\"bar\\f\\udead\\ubeef\""
-    genObj' bs i n = genObj' bs i 1 <> ", " <> genObj' bs (i-1) (n-1)
-
-
-
 
 build :: Builder -> ByteString
 build = L.toStrict . D.toLazyByteStringWith buildStrat L.empty
   where
-    buildStrat = D.untrimmedStrategy 128 256
+    buildStrat = D.untrimmedStrategy 2048 4096
     {-# INLINE buildStrat #-}
 {-# INLINE build #-}
-
-buildM :: MonadIO m => Builder -> BS.ByteString m ()
-buildM = BS.toStreamingByteStringWith buildStrat
-  where
-    buildStrat = D.untrimmedStrategy 128 256
-    {-# INLINE buildStrat #-}
-{-# INLINE buildM #-}
-
-
-
-extract :: A.Result (Maybe Builder) -> Int
-extract (A.Done _ (Just x)) = B.length $ build x
-extract _ = 0
-
-extract' :: Either String (Maybe Builder) -> Int
-extract' (Right (Just x)) = B.length $ build x
-extract' _ = 0
-
-keyToParser str =
-  let key = qkey [str]
-      ckey = mapClass key
-   in seekInObj ckey
-
-keyToParser' str =
-  let key = qkey [str]
-      ckey = mapClass key
-   in seekInObj' ckey
-
-keyToZepto str =
-  let key = qkey [str]
-      ckey = mapClass key
-   in seekInObjZepto ckey
-
-keyToStream str =
-  let key = qkey [str]
-      ckey = mapClass key
-   in seekInObjZeptoStream ckey

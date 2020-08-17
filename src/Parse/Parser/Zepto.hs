183ffcbbe071a0aa67cd1200fbfe83c4d7e39c6a
archaephyrryx <caufeminecraft+github@gmail.com>
2020-08-17 15:15:21 -0400

    Removed base monad from Zepto
    Reimplemented all of Parse.Parser.Zepto to remove the base monad
    m of `ZeptoT m a`, instead directly defining `Parser a`. All code
    based on Zepto in this library used a base monad of `Identity`
    in the monadic version, making this change sensible.

--- src/Parse/Parser/Zepto.hs
+++ src/Parse/Parser/Zepto.hs
@@ -13,10 +13,8 @@ module Parse.Parser.Zepto
   (
     Parser
   , Result(..)
-  , ZeptoT
   , parseR
   , parse
-  , parseT
   , atEnd
   , word8
   , string
@@ -42,9 +40,7 @@ import Prelude hiding (take, takeWhile)
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Unsafe as B
 
-newtype S = S {
-      input :: ByteString
-    }
+newtype S = S { input :: ByteString }
 
 data Result a = Fail String
               | OK !a S
@@ -54,164 +50,135 @@ data Result a = Fail String
 -- This monad is strict in its state, and the monadic bind operator
 -- ('>>=') evaluates each result to weak head normal form before
 -- passing it along.
-newtype ZeptoT m a = Parser {
-      runParser :: S -> m (Result a)
-    }
+newtype Parser a = Parser { runParser :: S -> Result a }
 
-type Parser a = ZeptoT Identity a
+instance Functor Parser where
+  fmap f m = Parser $ \s ->
+    case runParser m s of
+      OK a s'  -> OK (f a) s'
+      Fail err -> Fail err
+  {-# INLINE fmap #-}
 
-instance Monad m => Functor (ZeptoT m) where
-    fmap f m = Parser $ \s -> do
-      result <- runParser m s
-      case result of
-        OK a s'  -> return (OK (f a) s')
-        Fail err -> return (Fail err)
-    {-# INLINE fmap #-}
+instance Applicative Parser where
+  pure a = Parser $ OK a
+  {-# INLINE pure #-}
+  (<*>) = ap
+  {-# INLINE (<*>) #-}
 
-instance MonadIO m => MonadIO (ZeptoT m) where
-  liftIO act = Parser $ \s -> do
-    result <- liftIO act
-    return (OK result s)
-  {-# INLINE liftIO #-}
-
-instance Monad m => Monad (ZeptoT m) where
-    return = pure
-    {-# INLINE return #-}
-    m >>= k   = Parser $ \s -> do
-      result <- runParser m s
-      case result of
-        OK a s'  -> runParser (k a) s'
-        Fail err -> return (Fail err)
-    {-# INLINE (>>=) #-}
+instance Monad Parser where
+  return = pure
+  {-# INLINE return #-}
+  m >>= k   = Parser $ \s ->
+    case runParser m s of
+      OK a s'  -> runParser (k a) s'
+      Fail err -> Fail err
+  {-# INLINE (>>=) #-}
 
 #if !(MIN_VERSION_base(4,13,0))
-    fail = Fail.fail
-    {-# INLINE fail #-}
+  fail = Fail
+  {-# INLINE fail #-}
 #endif
 
-instance Monad m => Fail.MonadFail (ZeptoT m) where
-    fail msg = Parser $ \_ -> return (Fail msg)
-    {-# INLINE fail #-}
+instance Fail.MonadFail Parser where
+  fail msg = Parser $ \_ -> Fail msg
+  {-# INLINE fail #-}
 
-instance Monad m => MonadPlus (ZeptoT m) where
-    mzero = fail "mzero"
-    {-# INLINE mzero #-}
+instance MonadPlus Parser where
+  mzero = fail "mzero"
+  {-# INLINE mzero #-}
 
-    mplus a b = Parser $ \s -> do
-      result <- runParser a s
-      case result of
-        ok@(OK _ _) -> return ok
-        _           -> runParser b s
-    {-# INLINE mplus #-}
+  mplus a b = Parser $ \s ->
+    case runParser a s of
+      ok@(OK _ _) -> ok
+      _           -> runParser b s
+  {-# INLINE mplus #-}
 
-instance (Monad m) => Applicative (ZeptoT m) where
-    pure a = Parser $ \s -> return (OK a s)
-    {-# INLINE pure #-}
-    (<*>)  = ap
-    {-# INLINE (<*>) #-}
-
-gets :: Monad m => (S -> a) -> ZeptoT m a
-gets f = Parser $ \s -> return (OK (f s) s)
+gets :: (S -> a) -> Parser a
+gets f = Parser $ \s -> OK (f s) s
 {-# INLINE gets #-}
 
-put :: Monad m => S -> ZeptoT m ()
-put s = Parser $ \_ -> return (OK () s)
+put :: S -> Parser ()
+put s = Parser $ \_ -> OK () s
 {-# INLINE put #-}
 
 -- | Run a parser and return raw result
 parseR :: Parser a -> ByteString -> Result a
-parseR p bs = runIdentity (runParser p (S bs))
+parseR p bs = runParser p (S bs)
 {-# INLINE parseR #-}
 
 -- | Run a parser.
 parse :: Parser a -> ByteString -> Either String a
-parse p bs = case runIdentity (runParser p (S bs)) of
+parse p bs = case runParser p (S bs) of
                (OK a _)   -> Right a
                (Fail err) -> Left err
 {-# INLINE parse #-}
 
--- | Run a parser on top of the given base monad.
-parseT :: Monad m => ZeptoT m a -> ByteString -> m (Either String a)
-parseT p bs = do
-  result <- runParser p (S bs)
-  case result of
-    OK a _   -> return (Right a)
-    Fail err -> return (Left err)
-{-# INLINE parseT #-}
+instance Semigroup (Parser a) where
+  (<>) = mplus
+  {-# INLINE (<>) #-}
 
-instance Monad m => Semigroup (ZeptoT m a) where
-    (<>) = mplus
-    {-# INLINE (<>) #-}
+instance Mon.Monoid (Parser a) where
+  mempty  = fail "mempty"
+  {-# INLINE mempty #-}
+  mappend = (<>)
+  {-# INLINE mappend #-}
 
-instance Monad m => Mon.Monoid (ZeptoT m a) where
-    mempty  = fail "mempty"
-    {-# INLINE mempty #-}
-    mappend = (<>)
-    {-# INLINE mappend #-}
-
-instance Monad m => Alternative (ZeptoT m) where
-    empty = fail "empty"
-    {-# INLINE empty #-}
-    (<|>) = mplus
-    {-# INLINE (<|>) #-}
+instance Alternative Parser where
+  empty = fail "empty"
+  {-# INLINE empty #-}
+  (<|>) = mplus
+  {-# INLINE (<|>) #-}
 
 -- | Consume input while the predicate returns 'True'.
-takeWhile :: Monad m => (Word8 -> Bool) -> ZeptoT m ByteString
+takeWhile :: (Word8 -> Bool) -> Parser ByteString
 takeWhile p = do
   (h,t) <- gets (B.span p . input)
   put (S t)
   pure h
 {-# INLINE takeWhile #-}
-{-# SCC takeWhile #-}
 
 -- | Skip input while the predicate returns 'True'
-skipWhile :: Monad m => (Word8 -> Bool) -> ZeptoT m ()
+skipWhile :: (Word8 -> Bool) -> Parser ()
 skipWhile p = do
   t <- gets (B.dropWhile p . input)
   put (S t)
 {-# INLINE skipWhile #-}
-{-# SCC skipWhile #-}
 
 -- | Consume @n@ bytes of input.
-take :: Monad m => Int -> ZeptoT m ByteString
+take :: Int -> Parser ByteString
 take !n = do
   s <- gets input
   if B.length s >= n
     then put (S (B.unsafeDrop n s)) >> pure (B.unsafeTake n s)
     else fail "insufficient input"
 {-# INLINE take #-}
-{-# SCC take #-}
 
 -- | Skip @n@ bytes of input.
-skip :: Monad m => Int -> ZeptoT m ()
+skip :: Int -> Parser ()
 skip !n = do
   s <- gets input
   if B.length s >= n
     then put (S (B.unsafeDrop n s))
     else fail "insufficient input"
 {-# INLINE skip #-}
-{-# SCC skip #-}
 
-peek :: Monad m => ZeptoT m (Maybe Word8)
+peek :: Parser (Maybe Word8)
 peek = do
   s <- gets input
   if not $! B.null s
      then pure (Just $ B.unsafeHead s)
      else pure Nothing
 {-# INLINE peek #-}
-{-# SCC peek #-}
 
-pop :: Monad m => ZeptoT m Word8
+pop :: Parser Word8
 pop = do
   s <- gets input
   if not $! B.null s
      then put (S (B.unsafeTail s)) >> pure (B.unsafeHead s)
      else fail "insufficient input"
 {-# INLINE pop #-}
-{-# SCC pop #-}
 
-
-word8 :: Monad m => Word8 -> ZeptoT m ()
+word8 :: Word8 -> Parser ()
 word8 w = do
   i <- gets input
   if not $! B.null i
@@ -220,30 +187,27 @@ word8 w = do
       else fail "word8"
     else fail "insufficient input"
 {-# INLINE word8 #-}
-{-# SCC word8 #-}
 
 -- | Match a string exactly.
-string :: Monad m => ByteString -> ZeptoT m ()
+string :: ByteString -> Parser ()
 string s = do
   i <- gets input
   if s `B.isPrefixOf` i
     then put (S (B.unsafeDrop (B.length s) i))
     else fail "string"
 {-# INLINE string #-}
-{-# SCC string #-}
 
 -- | Match a string case-insensitively.
-stringCI :: Monad m => ByteString -> ZeptoT m ()
+stringCI :: ByteString -> Parser ()
 stringCI s = do
   i <- gets input
   let n = B.length s
   if n <= B.length i
-    then if s `lowEq` (B.unsafeTake n i)
+    then if s `lowEq` B.unsafeTake n i
       then put (S (B.unsafeDrop (B.length s) i))
       else fail "stringCI"
     else fail "insufficient input"
 {-# INLINE stringCI #-}
-{-# SCC stringCI #-}
 
 -- b1 and b2 must be of same non-zero length
 lowEq :: ByteString -> ByteString -> Bool
@@ -253,10 +217,9 @@ lowEq b1 b2 = B.map toLower b1 == B.map toLower b2
     toLower w | w >= 65 && w <= 90 = w + 32
               | otherwise          = w
 {-# INLINE lowEq #-}
-{-# SCC lowEq #-}
 
 -- | Indicate whether the end of the input has been reached.
-atEnd :: Monad m => ZeptoT m Bool
+atEnd :: Parser Bool
 atEnd = do
   i <- gets input
   pure $! B.null i

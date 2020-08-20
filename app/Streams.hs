306b55a76b4bccfd18e2a965a093aac3b9a7fd2c
archaephyrryx <caufeminecraft+github@gmail.com>
2020-08-20 18:52:16 -0400

    Cleaned unused imports from Streams module
    Removed unused imports in Streams module
    
    Requalified all monadic bytestrings appearing in signatures
    in terms of base library (imported as `BS`) rather than sub-library
    (imported as `BS8`)

--- app/Streams.hs
+++ app/Streams.hs
@@ -2,51 +2,31 @@
 
 module Streams where
 
-import Data.Semigroup ((<>))
-
 import Control.Monad.IO.Class (MonadIO(..))
 
-import qualified Data.ByteString as B
 import qualified Data.ByteString.Lazy as L
-import qualified Data.ByteString.Char8 as S8
-import Data.ByteString (ByteString)
-
-import Data.ByteString.Builder (Builder)
-import qualified Data.ByteString.Builder as D
-import qualified Data.ByteString.Builder.Extra as D
-
-import Data.Either (isLeft, fromLeft)
-
-import qualified Conduit as C
-import qualified Data.Conduit as C
-import qualified Data.Conduit.Combinators as C
-import Data.Conduit ((.|))
-
 import qualified Data.ByteString.Streaming as BS
 import qualified Data.ByteString.Streaming.Char8 as BS8
 
 import qualified Data.ByteString.Streaming.HTTP as H
-import Data.ByteString.Streaming.HTTP (MonadResource(..))
+import Data.ByteString.Streaming.HTTP (MonadResource(..), HttpException(..))
 
-import Streaming
+import           Streaming
+import           Streaming.Internal (Stream(..))
 import qualified Streaming.Prelude as S
-import Streaming.Internal (Stream(..))
-
 import qualified Streaming.Zip as Zip
 
-import qualified Parse.ReadAlt as Alt
-
-import qualified Parse.ReadZepto as Zep
-import qualified Parse.Parser.Zepto as Z
-
-import qualified Parse.ReadStream as ZepS
-import qualified Parse.Parser.ZeptoStream as ZS
-
-import Final (toVector, toVectorIO, toVectorsIO)
-import Global (nLines)
-
-import Data.Vector (Vector)
+import           Data.Vector (Vector)
 import qualified Data.Vector as V
+
+
+import Control.Exception.Base (try)
+
+
+-- local module imports
+
+import Final (toVectorsIO)
+import Global
 
 
 -- Newtypes for variants
@@ -92,34 +72,31 @@ fi :: a -> a -> Bool -> a
 fi x y p = if_ p x y
 {-# INLINE fi #-}
 
-
-
-
 -- | Generic function for producing a line-split input stream from a monadic bytestring produced
 --   via a specified transformation of a seed value
-linesOf :: MonadIO m => (a -> BS8.ByteString m ()) -> (a -> Stream (BS8.ByteString m) m ())
+linesOf :: MonadIO m => (a -> BS.ByteString m ()) -> (a -> Stream (BS.ByteString m) m ())
 linesOf f x = BS8.lines $ f x
 {-# INLINE linesOf #-}
 
 -- | Convert monadic bytestring to normalized raw format via conditional gunzip
-convertLines :: MonadIO m => BS8.ByteString m () -> InFormat -> Stream (BS8.ByteString m) m ()
+convertLines :: MonadIO m => BS.ByteString m () -> InFormat -> Stream (BS.ByteString m) m ()
 convertLines src = linesOf input
   where
     input = fi (Zip.gunzip src) src . isZipped
     {-# INLINE input #-}
 {-# INLINE convertLines #-}
 
-stdinLines :: MonadIO m => InFormat -> Stream (BS8.ByteString m) m ()
+stdinLines :: MonadIO m => InFormat -> Stream (BS.ByteString m) m ()
 stdinLines = convertLines BS.stdin
 {-# INLINE stdinLines #-}
 
-httpLines :: (MonadIO m, MonadResource m) => String -> InFormat -> Stream (BS8.ByteString m) m ()
+httpLines :: (MonadIO m, MonadResource m) => String -> InFormat -> Stream (BS.ByteString m) m ()
 httpLines url = convertLines (getHttp url)
 {-# INLINE httpLines #-}
 
-getHttp :: (MonadIO m, MonadResource m) => String -> BS8.ByteString m ()
+getHttp :: (MonadIO m, MonadResource m) => String -> BS.ByteString m ()
 getHttp url = rejoin $ do
-  req <-liftIO $ H.parseRequest url
+  req <- liftIO $ H.parseRequest url
   man <- liftIO $ H.newManager H.tlsManagerSettings
   H.http req man
 
